32052507aa766e698058e1ea9007bfae2b0de02f
Elie Marot <elie.marot@gmail.com>
2015-08-26 11:02:36 +0200

    add user form

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
 
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :mail, format: { with: /\A.*@.*\.com\z/ }, uniqueness: true
+  validates :mail, format: { with: /[a-zA-Z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}/ }, uniqueness: true
   validates :address, presence: true
   validates :description, length: { minimum: 10 }
 
