8f39c9757687b1c1d90585910ee62db6426f80f0
Elie Marot <elie.marot@gmail.com>
2015-08-26 15:54:09 +0200

    login + navbar

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,10 @@ class User < ActiveRecord::Base
   has_many :reviews
   has_many :projects
 
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :mail, format: { with: /[a-zA-Z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}/ }, uniqueness: true
-  validates :address, presence: true
-  validates :description, length: { minimum: 10 }
+  # validates :first_name, presence: true
+  # validates :last_name, presence: true
+  # validates :email, format: { with: /[a-zA-Z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}/ }, uniqueness: true
+  # validates :address, presence: true
+  # validates :description, length: { minimum: 10 }
 
 end
